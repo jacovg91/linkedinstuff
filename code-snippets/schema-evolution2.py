24f840937df9ba8e200e29a91661309b088bbb08
Jaco van Gelder <jaco.van.gelder@inter.ikea.com>
2024-09-12 14:28:14 +0200

    added return_schema_evolution function

---
+++ code-snippets/schema-evolution2.py
@@ -0,0 +1,51 @@
+from pyspark.sql import SparkSession, DataFrame
+from typing import List
+
+def return_schema_evolution(full_table_names: List[str]) -> DataFrame:
+    """
+    Returns the schema evolution of a list of tables passed.
+
+    Args:
+        full_table_names (List[str]): A list of full table names, in format:
+                                      'catalog.schema.table'
+
+    Returns:
+        df (DataFrame): A DataFrame containing schema evolution for passed tables.
+    """
+    spark = SparkSession.getActiveSession()
+    sql_to_run = """
+    WITH CTE_HIST
+    AS
+    (
+         """ + ' UNION ALL '.join([f"""SELECT '{table}' AS table_name,
+          * FROM (DESCRIBE HISTORY {table})""" for table in full_table_names]) + """
+    ),
+    CTE_STRUCT
+    AS (
+        SELECT *
+        ,      explode(from_json(operationParameters.columns,
+                            'array<struct<column:struct<name:string>>>'
+                            ).column.name) AS column
+        FROM CTE_HIST
+        WHERE operation = 'ADD COLUMNS'
+        UNION ALL
+        SELECT *
+        ,      explode(from_json(operationParameters.columns,
+                            'array<string>'
+                            )) AS column
+        FROM CTE_HIST
+        WHERE operation = 'DROP COLUMNS'
+    )
+    SELECT table_name AS table_name 
+    ,      column AS column
+    ,      IF(operation = 'ADD COLUMNS', 'ADDED', 'REMOVED') AS added_or_removed
+    ,      timestamp AS schema_evolution_date_time
+    ,      version AS changed_by_delta_version
+    ,      userName AS changed_by_user
+    ,      job.jobId AS changed_by_job_id
+    ,      job.jobName AS changed_by_job_name
+    FROM CTE_STRUCT
+    """
+    df = spark.sql(sql_to_run)
+
+    return df
